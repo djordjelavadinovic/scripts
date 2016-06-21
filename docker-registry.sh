48ba7b7fba96595b870ba77009b25dddbfcb7b78
root <root@docker1.antegra.com>
2016-06-21 09:26:42 -0400

    Few scipts

---
+++ docker-registry.sh
@@ -0,0 +1,5 @@
+docker run -d -p 5000:5000 --restart=always --name registry \
+  -v /root/certs:/certs \
+  -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt \
+  -e REGISTRY_HTTP_TLS_KEY=/certs/domain.key \
+  registry:2
